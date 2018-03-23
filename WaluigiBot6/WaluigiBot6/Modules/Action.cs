24c103e543f8ec32d6b7b55d7fd60d8deadeaffa
kahcheong <kc16rr@brocku.ca>
2018-03-23 03:44:38 -0400

    working slap

---
+++ WaluigiBot6/WaluigiBot6/Modules/Action.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaluigiBot6.Modules
+{
+    
+    public class Action : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("slap")]
+        public async Task Slap(SocketGuildUser user)
+        {
+            var builder = new EmbedBuilder();
+
+            builder.WithTitle($"{user.Username}, you got a slap from {Context.User.Username}");
+            builder.WithThumbnailUrl("https://cdn.weeb.sh/images/S1lf3XkKvW.gif");
+            builder.WithColor(Color.Blue);
+
+            await Context.Channel.SendMessageAsync("", false, builder);
+
+        }
+    }
+}
